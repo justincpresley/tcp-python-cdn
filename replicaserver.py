918242717604c28cab2f2a8836f1e72354f7e188
embersight <justincpresley@gmail.com>
2020-11-20 02:04:56 -0600

    changes

--- replicaserver.py
+++ replicaserver.py
@@ -43,7 +43,7 @@ class ClientThread(threading.Thread):
                 send_packet(self.socket,form_packet(ack_num_from_packet(packet), seq_num_from_packet(packet)+1,bytes, syn=True, fin=True))
                 logging.info(f'SEND   {ack_num_from_packet(packet)} {seq_num_from_packet(packet)+1} ACK={"Y "if False else "N"} SYN={"Y" if True else "N"} FIN={"Y" if True else "N"}')
                 packet = receive_packet(self.socket)
-                logging.info(f'RECV   {syn_num} {ack_num} ACK={"Y "if ack else "N"} SYN={"Y" if syn else "N"} FIN={"Y" if fin else "N"}')
+                logging.info(f'RECV   {syn_num_from_packet(packet)} {ack_num_from_packet(packet)} ACK={"Y "if ack else "N"} SYN={"Y" if syn else "N"} FIN={"Y" if fin else "N"}')
 
         self.socket.close()
         logging.info(f'[-] Thread ended for {self.ip}, {str(self.port)}')
